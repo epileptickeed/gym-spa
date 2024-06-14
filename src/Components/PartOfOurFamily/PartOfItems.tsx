e0dee4c4cace771e4c35356f83d57b97c02da4c7
viktorpc <kurskiycson26@yandex.ru>
2024-06-14 15:06:40 +0300

    fix

--- src/Components/PartOfOurFamily/PartOfItems.tsx
+++ src/Components/PartOfOurFamily/PartOfItems.tsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-
+import { motion } from 'framer-motion';
 
 type PartItemsProps = {
-    img: string,
-    id: number
-}
+  img: string;
+  id: number;
+};
 
 const PartOfItems = ({ img, id }: PartItemsProps) => {
-
-
-    const vars = {
-        hidden: { opacity: 0, y: 50},
-        show: (id: number) => ({ 
-            opacity: 1,
-            y: 0,
-            transition: {
-                delay: 0.10 * id
-            }
-        })
-    }
+  const vars = {
+    hidden: { opacity: 0, y: 50 },
+    show: (id: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.1 * id,
+      },
+    }),
+  };
 
   return (
     <motion.div
-        variants={vars} custom={id} initial='hidden' whileInView='show' transition={{ delay: 0.5}} viewport={{ once:true }}
-    >
-        <img src={img} alt="" />
+      variants={vars}
+      custom={id}
+      initial="hidden"
+      whileInView="show"
+      transition={{ delay: 0.5 }}
+      viewport={{ once: true }}>
+      <img src={img} alt="" />
     </motion.div>
-  )
-}
+  );
+};
 
-export default PartOfItems
+export default PartOfItems;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,28 +1,25 @@
-import Header from '../Components/Header'
-import BeStronger from '../Components/BeStronger'
-import DayToStart from '../Components/DayToStart'
-import Brands from '../Components/Brands/Brands'
-import JoinUs from '../Components/JoinUs'
-import SignUpForm from '../Components/SignUpForm'
-import FindUs from '../Components/FindUs/FindUs'
-import PartOfOurFamily from '../Components/PartOfOurFamily/PartOfOurFamily'
-import YourBmi from '../Components/YourBmi'
+import Header from '../Components/Header';
+import BeStronger from '../Components/BeStronger';
+import DayToStart from '../Components/DayToStart';
+import Brands from '../Components/Brands/Brands';
+import JoinUs from '../Components/JoinUs';
+import SignUpForm from '../Components/SignUpForm';
+import PartOfOurFamily from '../Components/PartOfOurFamily/PartOfOurFamily';
+import YourBmi from '../Components/YourBmi';
 
 const Home = () => {
-
   return (
     <div>
       <Header />
       <BeStronger />
       <DayToStart />
-      <Brands /> 
+      <Brands />
       <JoinUs />
       <SignUpForm />
-      <FindUs />
       <PartOfOurFamily />
       <YourBmi />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
